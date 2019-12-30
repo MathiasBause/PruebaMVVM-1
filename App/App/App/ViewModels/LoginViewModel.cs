e028db3cb8e3f9f860df7a3d617b28b57202af53
gmathiasbll <gmathiasbll@gmail.com>
2019-12-30 00:18:29 -0400

    ViewModels e Interfaces

---
+++ App/App/App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,67 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace App.ViewModels
+{
+    class LoginViewModel:BaseViewModel
+    {
+        #region Variables
+        string email;
+        string password;
+        bool isrunning;
+        bool isenabled;
+        #endregion
+
+        #region Properties
+        public string Email {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+        public string Password {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+        public bool IsRunning {
+            get { return this.isrunning; }
+            set { SetValue(ref this.isrunning, value); }
+        }
+        public bool IsEnabled
+        {
+            get { return this.isenabled; }
+            set { SetValue(ref this.isenabled, value); }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand { 
+            get 
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (String.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Email empty",
+                                                        "Please fill in the E-mail",
+                                                        "Accept");
+                return;
+            }
+            if (String.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Password empty",
+                                                        "Please fill in the password",
+                                                        "Accept");
+                return;
+            }
+
+            IsRunning = true;
+        }
+        #endregion
+    }
+}
